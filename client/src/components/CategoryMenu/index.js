4bb890f1d4f379ab9c7e92c7411f9d27b2eb8dc7
Muhammad Daaboul <muhammad.a.daaboul@gmail.com>
2022-04-15 23:25:27 +0100

    refactor updateCurrentCategory to react-redux

--- client/src/components/CategoryMenu/index.js
+++ client/src/components/CategoryMenu/index.js
@@ -1,44 +1,41 @@
 import React, { useEffect } from 'react';
 import { useQuery } from '@apollo/client';
-import { useStoreContext } from '../../utils/GlobalState';
+// import { useStoreContext } from '../../utils/GlobalState';
+// import {
+//   UPDATE_CATEGORIES,
+//   UPDATE_CURRENT_CATEGORY,
+// } from '../../utils/actions';
+import { useDispatch, useSelector } from 'react-redux';
 import {
-  UPDATE_CATEGORIES,
-  UPDATE_CURRENT_CATEGORY,
-} from '../../utils/actions';
+  updateCategories,
+  updateCurrentCategory,
+} from '../../features/product/productSlice';
 import { QUERY_CATEGORIES } from '../../utils/queries';
 import { idbPromise } from '../../utils/helpers';
 
 function CategoryMenu() {
-  const [state, dispatch] = useStoreContext();
-
-  const { categories } = state;
+  const dispatch = useDispatch();
+  const { categories } = useSelector((store) => store.product);
+  // const [state, dispatch] = useStoreContext();
+  // const { categories } = state;
 
   const { loading, data: categoryData } = useQuery(QUERY_CATEGORIES);
 
   useEffect(() => {
     if (categoryData) {
-      dispatch({
-        type: UPDATE_CATEGORIES,
-        categories: categoryData.categories,
-      });
+      dispatch(updateCategories({ categories: categoryData.categories }));
       categoryData.categories.forEach((category) => {
         idbPromise('categories', 'put', category);
       });
     } else if (!loading) {
       idbPromise('categories', 'get').then((categories) => {
-        dispatch({
-          type: UPDATE_CATEGORIES,
-          categories: categories,
-        });
+        dispatch(updateCategories({ categories }));
       });
     }
   }, [categoryData, loading, dispatch]);
 
   const handleClick = (id) => {
-    dispatch({
-      type: UPDATE_CURRENT_CATEGORY,
-      currentCategory: id,
-    });
+    dispatch(updateCurrentCategory({ currentCategory: id }));
   };
 
   return (
