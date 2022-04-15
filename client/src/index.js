91ca486f374c8ac5fb890a5bddc8a2bb33570f1f
Muhammad Daaboul <muhammad.a.daaboul@gmail.com>
2022-04-15 17:33:24 +0100

    added store.js and installed redux toolkit

--- client/src/index.js
+++ client/src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { store } from './store';
+// this is the app (i.e. react-redux) that connects redux store (aka redux) with our application
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* similar to context api, we need to wrap the app using the Provider, but in this case takes in a store prop */}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
